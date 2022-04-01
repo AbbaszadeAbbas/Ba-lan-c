672b576c73a71903bb7a35a5efd7cf597ef2410e
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-04-01 11:51:09 +0500

    jsdatastructures

--- Works/JsDataStructures/app.js
+++ Works/JsDataStructures/app.js
@@ -33,3 +33,32 @@
 // }
 
 // Abbas()
+
+// // 3 telebenin xususiyyetleri
+
+// let adlar = ['Ehmed' , 'Memmed' , 'Sahil']
+// let soyadlar = ['Ehmedov' , 'Qasimov' , 'Haciyev']
+// let telebeler = [adlar, soyadlar]
+
+// console.log(telebeler[0][0] , telebeler[1][0])
+
+// // telebeler
+
+// let telebe01 = {
+//   ad: "Ayxan" ,
+//   soyad: "Huseynli"
+// }
+
+// let telebe02 = {
+//   ad: "Elcan" ,
+//   soyad: "Muradov"
+// }
+
+// let telebe03 = {
+//   ad: "Samir" ,
+//   soyad: "Kerimov"
+// }
+
+// let telebeler = [telebe01 , telebe02 , telebe03]
+
+// console.log(telebeler[0])
