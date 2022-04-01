80cbe138ddfa03a39cce570cc7c5e5aaeaf2642e
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-04-01 13:07:53 +0500

    tv01 tv02

--- Works/JsDataStructures/app.js
+++ Works/JsDataStructures/app.js
@@ -80,6 +80,12 @@
 // console.log(`${telebeler[0][1]} \ ${telebeler[1][1]}`)
 // console.log(`${telebeler[0][2]} \ ${telebeler[1][2]}`)
 
+
+
+
+//                                                TASKS
+
+
 // // tv01
 // let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
 // let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
@@ -91,15 +97,15 @@
 // }
 
 // // tv02
-let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
-let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
-let ages = [23, 34, 40, 58, 60];
-let students = {
-    adlar: names,
-    soyadlar: surnames,
-    yaslar: ages
-}
+// let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
+// let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
+// let ages = [23, 34, 40, 58, 60];
+// let students = {
+//     adlar: names,
+//     soyadlar: surnames,
+//     yaslar: ages
+// }
 
-for (i=0 ; i<5 ; i++) {
-  console.log(`Ad: ${students["adlar"][i]} , Soyad: ${students["soyadlar"][i]} , Yas: ${students["yaslar"][i]}`)
-}
+// for (i=0 ; i<5 ; i++) {
+//   console.log(`Ad: ${students["adlar"][i]} , Soyad: ${students["soyadlar"][i]} , Yas: ${students["yaslar"][i]}`)
+// }
