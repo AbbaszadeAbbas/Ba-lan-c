0033ab2d560b79bfa7fe330660ae75e21cf1a103
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-04-11 16:53:52 +0300

    last task

--- Works/VanillaJsFilteringData/Countries/Country01.js
+++ Works/VanillaJsFilteringData/Countries/Country01.js
@@ -1,3 +1,37 @@
-function Country() {
-  //
+let students = [{
+  ad: 'Eli',
+  soyad: 'Piriyev',
+  yas: 23
+}, {
+  ad: 'Ehmed',
+  soyad: 'Memmedov',
+  yas: 34
+}, {
+  ad: 'Sara',
+  soyad: 'Qurbanova',
+  yas: 40
+}, {
+  ad: 'Sevda',
+  soyad: 'Ezizova',
+  yas: 58
+}, {
+  ad: 'Qurban',
+  soyad: 'Sadiqzade',
+  yas: 60
+}]
+
+function GetData(_obj) {
+  _objData = ""
+  for(let _key in _obj){
+    _objData += `${_key}: ${_obj[_key]}, `
+  }
+  return _objData
 }
+
+function GetAllData(_arr){
+  for(i=0 ; i<_arr.length ; i++){
+    console.log(GetData(_arr[i]))
+  }
+}
+
+GetAllData(students)
