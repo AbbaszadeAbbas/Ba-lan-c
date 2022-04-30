8a6e88b5abe852b9521fee615caec6eeceb3e7dd
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-04-30 23:57:45 +0400

    hackerrank trial

---
+++ Works/HackerRank/staircase.js
@@ -0,0 +1,21 @@
+let a = " ";
+let b = "#";
+
+  let cem1 = a+a+a+a+a+b
+  let cem2 = a+a+a+a+b+b
+  let cem3 = a+a+a+b+b+b
+  let cem4 = a+a+b+b+b+b
+  let cem5 = a+b+b+b+b+b
+  let cem6 = b+b+b+b+b+b
+console.log(cem1);
+console.log(cem2);
+console.log(cem3);
+console.log(cem4);
+console.log(cem5);
+console.log(cem6);
+
+
+let n = 5;
+let i = n;
+
+for(){}
