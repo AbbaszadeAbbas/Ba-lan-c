7406c2f6e3c129d669e9e40f242faae62751c574
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-01 14:06:38 +0400

    hacker rank staircase

--- Works/HackerRank/staircase.js
+++ Works/HackerRank/staircase.js
@@ -1,21 +1,29 @@
 let a = " ";
 let b = "#";
 
-  let cem1 = a+a+a+a+a+b
-  let cem2 = a+a+a+a+b+b
-  let cem3 = a+a+a+b+b+b
-  let cem4 = a+a+b+b+b+b
-  let cem5 = a+b+b+b+b+b
-  let cem6 = b+b+b+b+b+b
-console.log(cem1);
-console.log(cem2);
-console.log(cem3);
-console.log(cem4);
-console.log(cem5);
-console.log(cem6);
+// let cem1 = a + a + a + a + a + b
+// let cem2 = a + a + a + a + b + b
+// let cem3 = a + a + a + b + b + b
+// let cem4 = a + a + b + b + b + b
+// let cem5 = a + b + b + b + b + b
+// let cem6 = b + b + b + b + b + b
 
 
-let n = 5;
-let i = n;
+// let umumi = [
+//   console.log(cem1),
+//   console.log(cem2),
+//   console.log(cem3),
+//   console.log(cem4),
+//   console.log(cem5),
+//   console.log(cem6)
+// ]
 
-for(){}
+function staircase(n) {
+  let i = 0;
+  let j = n;
+  for(i<n;i++ && j<n;j--){
+    
+  }
+}
+
+staircase(2)
