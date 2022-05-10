d081d5df9bbae89052a3761e7b209df7c8b94b60
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-11 01:41:53 +0400

    UI design

---
+++ Works/LoginInfo/js/app.js
@@ -0,0 +1,9 @@
+function Yuxari() {
+  document.getElementsByClassName("box-inside").style.transform =
+    " translateY(580px);";
+}
+
+function Asagi() {
+  document.getElementsByClassName("box-inside").style.transform =
+    " translateY(-600px);";
+}
