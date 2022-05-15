fbe74687c13de77888f368ab7fddb22f0921dcc8
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-15 21:27:01 +0400

    python f'string and .format() method

---
+++ ProjectBackEnd/python/app.py
@@ -0,0 +1,13 @@
+# print("Hello World");
+
+# # F'string
+
+# name = 'dunya';
+# message = f"merhaba  {name}";
+# print(message)
+
+# # Format method
+
+# name = "Abbas";
+# message = "adim:{}";
+# print(message.format(name))
