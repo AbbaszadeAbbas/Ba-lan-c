7d4dba113fd6282a7c25b4f1fcde5fab155223ac
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-15 21:43:34 +0400

    python title() method

---
+++ ProjectBackEnd/python/day_1.py
@@ -0,0 +1,31 @@
+# print("Hello World");
+
+# # F'string
+
+# bu metoddan stringe basqa stringi elave etmek ucun istifade olunur
+
+# name = 'dunya';
+# message = f"merhaba  {name}";
+# print(message)
+
+# # Format method
+
+# bu metoddan stringe basqa stringi elave etmek ucun istifade olunur
+
+# name = "Abbas";
+# message = "adim:{}";
+# print(message.format(name))
+
+# # len() method
+
+# bu metoddan verilen stringdeki soz( ve ya sozlerin ) uzunlugunu olcmek ucun istifade olunur
+
+# name = "Abbas";
+# print(len(name));
+
+# # title() method
+
+# bu metoddan stringdeki sozlerin bas herfini boyutmek ucun istifade olunur.
+
+# name = "hello world";
+# print(name.title())
