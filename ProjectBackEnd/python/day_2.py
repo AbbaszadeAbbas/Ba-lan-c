82b1b9ddfb22233a4636d16f098373c6e30c6803
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-16 14:36:31 +0400

    type() method in python

---
+++ ProjectBackEnd/python/day_2.py
@@ -0,0 +1,9 @@
+# print("Let's GO!");
+
+# # # #     type() method
+
+# bu metoddan verilen inputun tipini oyrenmek ucun istifade olunur.
+
+# name = 'dunya';
+# print(type(name))   # expected output: <class 'str'>
+
