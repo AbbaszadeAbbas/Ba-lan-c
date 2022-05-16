ee3045784589f345aeb9f6750531f0e4afbb98af
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-16 15:30:05 +0400

    pow() method in python

--- ProjectBackEnd/python/day_2/app.py
+++ ProjectBackEnd/python/day_2/app.py
@@ -9,47 +9,58 @@
 
 # # # #     Math in code
 
-# Toplama
+# # Toplama
 
 # toplama = 1+5;
 # print(toplama)   # expected output: 6
 
-# Cixma
+# # Cixma
 
 # cixma = 5-9;
 # print(cixma);   # expected output: 4
 
-# Vurma
+# # Vurma
 
 # vurma = 5*8;
 # print(vurma);   # expected output: 40
 
-# Tam bolme
+# # Tam bolme
 
 # tam_bolme = 6/3;  # bu bolme vasitesi ile bolunmeden alinan neticenin hem tam hissesi hemde kesr hissesi cap edilir.
 # print(tam_bolme);   # expected output: 2.0
 
-# Tam hisseni tapmaqla bolme
+# # Tam hisseni tapmaqla bolme
 
 # yuvarlaq_bolme = 6//3;  # bu bolme vasitesi ile bolunmeden alinan neticenin sadece tam hissesi cap edilir.
 # print(yuvarlaq_bolme)   # expected output:2
 
-# Quvvete yukseltme
+# # Quvvete yukseltme
 
 # quvvet = 5 ** 2;  # burda qosa ulduzdan sonra yazilan eded quvveti temsil edir.
 # print(quvvet)   # expected output: 25
 
-# Qaliq tapma
+# # Qaliq tapma
 
 # qaliq_tapma = 14%5; # bu metod vasitesi ile 1ci daxil edilen eded 2ciye bolunur ve qaliq cap edilir.
 # print(qaliq_tapma)   # expected output: 4
 
-# Ededin modulunu tapma
+# # # #     abs() method
 
-# modul_tapma = -15; # bu metod vasitesi ile daxil edilen ededin modulu tapilir.
+# bu metod vasitesi ile daxil edilen ededin modulu tapilir.
+
+# modul_tapma = -15;
 # print(abs(modul_tapma))   # expected output: 15
 
-# Ededi yuvarlaqlasdirma
+# # # #     round() method
+
+# bu metoddan verilen ededi yuvarlaqlasdirmaq ucun istifade olunur.
 
 # yuvarlaq = 6.797850376;
 # print(round(yuvarlaq));   # expected output: 7
+
+# # # #     pow() method
+
+# bu metoddan quvvete yukseltmek ucun istifade olunur.
+# burda 1ci qiymet eded 2ci qiymet quvveti gosterir.
+
+# print(pow(3, 2))   # expected output: 9
