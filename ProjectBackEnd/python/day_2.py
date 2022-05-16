f59f7c07747a27691243a610acd2ca84490694d7
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-16 14:53:23 +0400

    math in python

--- ProjectBackEnd/python/day_2.py
+++ ProjectBackEnd/python/day_2.py
@@ -7,3 +7,39 @@
 # name = 'dunya';
 # print(type(name))   # expected output: <class 'str'>
 
+# # # #     Math in code
+
+# Toplama
+
+# toplama = 1+5;
+# print(toplama)
+
+# Cixma
+
+# cixma = 5-1;
+# print(cixma);
+
+# Vurma
+
+# vurma = 5*8;
+# print(vurma);
+
+# Tam bolme
+
+# tam_bolme = 6/3;
+# print(tam_bolme);
+
+# Yuvarlaq bolme
+
+# yuvarlaq_bolme = 6//3;  # bu bolme vasitesi ile bolunmeden alinan neticenin sadece tam hissesi cap edilir.
+# print(yuvarlaq_bolme)
+
+# Quvvete yukseltme
+
+# quvvet = 5 ** 2;  # burda qosa ulduzdan sonra yazilan eded quvveti temsil edir.
+# print(quvvet)
+
+# Qaliq tapma
+
+# qaliq_tapma = 14%5; # bu metod vasitesi ile 1ci daxil edilen eded 2ciye bolunur ve qaliq cap edilir.
+# print(qaliq_tapma)
