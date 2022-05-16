c569aa24e488a821053b0eb3d7c87b5ea3acb5f6
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-16 15:04:21 +0400

    abs() method in python

--- ProjectBackEnd/python/day_2/app.py
+++ ProjectBackEnd/python/day_2/app.py
@@ -12,34 +12,39 @@
 # Toplama
 
 # toplama = 1+5;
-# print(toplama)
+# print(toplama)   # expected output: 6
 
 # Cixma
 
-# cixma = 5-1;
-# print(cixma);
+# cixma = 5-9;
+# print(cixma);   # expected output: 4
 
 # Vurma
 
 # vurma = 5*8;
-# print(vurma);
+# print(vurma);   # expected output: 40
 
 # Tam bolme
 
-# tam_bolme = 6/3;
-# print(tam_bolme);
+# tam_bolme = 6/3;  # bu bolme vasitesi ile bolunmeden alinan neticenin hem tam hissesi hemde kesr hissesi cap edilir.
+# print(tam_bolme);   # expected output: 2.0
 
 # Yuvarlaq bolme
 
 # yuvarlaq_bolme = 6//3;  # bu bolme vasitesi ile bolunmeden alinan neticenin sadece tam hissesi cap edilir.
-# print(yuvarlaq_bolme)
+# print(yuvarlaq_bolme)   # expected output:2
 
 # Quvvete yukseltme
 
 # quvvet = 5 ** 2;  # burda qosa ulduzdan sonra yazilan eded quvveti temsil edir.
-# print(quvvet)
+# print(quvvet)   # expected output: 25
 
 # Qaliq tapma
 
 # qaliq_tapma = 14%5; # bu metod vasitesi ile 1ci daxil edilen eded 2ciye bolunur ve qaliq cap edilir.
-# print(qaliq_tapma)
+# print(qaliq_tapma)   # expected output: 4
+
+# Ededin modulunu tapma
+
+# modul_tapma = -15; # bu metod vasitesi ile 1ci daxil edilen ededin modulu tapilir.
+# print(abs(modul_tapma))   # expected output: 15
