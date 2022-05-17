4bd69f3e0b98bf0d02ad836dad1b6c8bd294031d
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-17 20:53:53 +0400

    min() method in python

--- ProjectBackEnd/python/day_3/app.py
+++ ProjectBackEnd/python/day_3/app.py
@@ -4,3 +4,11 @@
 
 number = 1,2,3,4,5;
 print(max(number))   # expected output: 5
+
+
+# # # #     min() method
+
+# bu metoddan verilen inputun icindeki ededlerin en kiciyini secmek ucun istifade olunur.
+
+number = 1,2,3,4,5;
+print(min(number))   # expected output: 1
