758bebd92ec2ef50c3f81a7b241e78bb5e2e58ca
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-17 20:54:25 +0400

    int() method in python

--- ProjectBackEnd/python/day_3/app.py
+++ ProjectBackEnd/python/day_3/app.py
@@ -12,3 +12,16 @@ print(max(number))   # expected output: 5
 
 number = 1,2,3,4,5;
 print(min(number))   # expected output: 1
+
+
+# # # #     int() method
+
+# bu metoddan verilen stringi number a cevirmek ucundur.
+
+num__1 = "15";
+num__2 = "16";
+
+num_1 = int(num__1);
+num_2 = int(num__2);
+
+print(num_1+num_2)   # expected output: 31
