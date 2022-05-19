b7402a4acbc061e3dbce1865ee5b1cddbb0ba074
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-19 23:55:51 +0400

    sort() method in python

--- ProjectBackEnd/python/day_4/app.py
+++ ProjectBackEnd/python/day_4/app.py
@@ -56,3 +56,15 @@ print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir'];
 cities.remove('Mingecevir');
 
 print(cities);   # expected output: ['Baki', 'Gence'];
+
+
+# # # #     sort()  method
+
+# bu metoddan verilen listin icerisindeki istenilen elementleri elifba sirasina gore duzmek ucun istifade olunur.
+
+countries = ["Russia" , "Azerbaijan" , "Greece" , "Poland"];
+print(countries);   # expected output: ['Russia', 'Azerbaijan', 'Greece', 'Poland'];
+
+countries.sort();
+
+print(countries)   # expected output: ['Azerbaijan', 'Greece', 'Poland', 'Russia'];
