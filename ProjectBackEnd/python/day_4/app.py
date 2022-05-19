89f055ebc3e06b1de3589c67465abbdf1fb698b6
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-19 23:38:48 +0400

    del method in python

--- ProjectBackEnd/python/day_4/app.py
+++ ProjectBackEnd/python/day_4/app.py
@@ -20,3 +20,15 @@ print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir']
 cities.insert(2,"Sumqayit");   #1ci daxil edilen eded daxil edilecek elementin yerini bildirir.
 
 print(cities);   # expected output: ['Baki', 'Gence', 'Sumqayit', 'Mingecevir']
+
+
+# # # #     del  method
+
+# bu metoddan verilen listin icerisindeki hansisa elementi silmek ucun istifade olunur.
+
+cities = ['Baki','Gence','Mingecevir'];
+print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir'];
+
+del cities[2]
+
+print(cities);   # expected output: ['Baki', 'Gence'];
