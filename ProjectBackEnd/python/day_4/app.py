076115ccd19147e89fbb10075ce96f2721eadbdf
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-19 23:29:55 +0400

    insert() method in python

--- ProjectBackEnd/python/day_4/app.py
+++ ProjectBackEnd/python/day_4/app.py
@@ -8,3 +8,15 @@ print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir']
 cities.append("Sumqayit");
 
 print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir', 'Sumqayit']
+
+
+# # # #     insert() method
+
+# bu metoddan verilen listin istenilen yerine yeni element daxil etmek ucun istifade olunur.
+
+cities = ['Baki','Gence','Mingecevir'];
+print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir']
+
+cities.insert(2,"Sumqayit");   #1ci daxil edilen eded daxil edilecek elementin yerini bildirir.
+
+print(cities);   # expected output: ['Baki', 'Gence', 'Sumqayit', 'Mingecevir']
