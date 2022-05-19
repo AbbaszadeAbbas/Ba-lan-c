e226823c8175bea61ad3e754649e96a4d4921745
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-19 23:43:08 +0400

    pop() method in python

--- ProjectBackEnd/python/day_4/app.py
+++ ProjectBackEnd/python/day_4/app.py
@@ -32,3 +32,15 @@ print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir'];
 del cities[2]
 
 print(cities);   # expected output: ['Baki', 'Gence'];
+
+
+# # # #     pop()  method
+
+# bu metoddan verilen listin icerisindeki sonuncu elementi silmek ucun istifade olunur.
+
+cities = ['Baki','Gence','Mingecevir'];
+print(cities);   # expected output: ['Baki', 'Gence', 'Mingecevir'];
+
+cities.pop();
+
+print(cities);   # expected output: ['Baki', 'Gence'];
