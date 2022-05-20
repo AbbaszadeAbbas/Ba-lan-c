7ea41918b044b2a012fff91b1d063ac91dea05ff
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-20 23:16:48 +0400

    quiz test

--- Works/ProjectQuiz/assets/js/app.js
+++ Works/ProjectQuiz/assets/js/app.js
@@ -1,10 +1,29 @@
 const insideBox = document.querySelector(".com");
 const  error1   = document.querySelector(".error1");
 const  error2   = document.querySelector(".error2");
-const  inpt1    = document.querySelector(".inpt1");
-const  inpt2    = document.querySelector(".inpt2");
-const login_button = document.querySelector(".login-button")
+const  email    = document.querySelector("#email");
+const  password    = document.querySelector("#password");
+const login_button = document.querySelector("#login_button")
 
+login_button.addEventListener('click' , login)
+
+function login(){
+ if(email.value == ""){
+   error1.style.visibility = "visible"
+ }else{
+  error1.style.visibility = "hidden"
+ }
+
+  if(password.value == ""){
+   error2.style.visibility = "visible"
+ }else{
+  error2.style.visibility = "hidden" 
+ }
+
+ if(password.value !== "" && email.value !== ""){
+   alert("Login Success")
+ }
+}
 
 function Yuxari() {
   insideBox.style.transform = "translateY(580px)"
@@ -19,18 +38,3 @@ function Sifirla() {
   insideBox.style.transform = "translateY(0px)"
 }
 
-login_button.addEventListener('click' , function(e) {
-  e.preventDefault();
-
-  if(inpt1.value == ""){
-    error1.style.visibility = "visible"
-  }else{
-    error1.style.visibility = "hidden"
-  }
-
-  if(inpt2.value == ""){
-    error2.style.visibility = "visible"
-  }else{
-    error2.style.visibility = "hidden"
-  }
-})
