55a9b4a7062c7467c274751938f240f2b1ed72c4
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-20 14:38:38 +0400

    in method in python

--- ProjectBackEnd/python/day_5/app.py
+++ ProjectBackEnd/python/day_5/app.py
@@ -5,3 +5,15 @@
 cities = ["Sumqayit" , "Baki" , "Zengilan"];
 
 print(cities.index("Sumqayit"));   # expected output: 0
+
+
+
+# # # #     in  method
+
+# bu metoddan verilen listin icerisindeki hansisa elementin olub-olmadigini yoxlamaq ucunn istifade olunur.
+
+cities = ["Sumqayit" , "Baki" , "Zengilan"];
+
+print("Sumqayit" in cities);   # expected output: True
+
+print("London" in cities);     # expected output: False
