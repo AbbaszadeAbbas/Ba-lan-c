3c097f258fbe88d593ba5dae74390c285df2deb9
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-20 14:38:17 +0400

    index() method in python

---
+++ ProjectBackEnd/python/day_5/app.py
@@ -0,0 +1,7 @@
+# # # #     index()  method
+
+# bu metoddan verilen listin icerisindeki hansisa elementin indexini oyrenmek ucun istifade olunur.
+
+cities = ["Sumqayit" , "Baki" , "Zengilan"];
+
+print(cities.index("Sumqayit"));   # expected output: 0
