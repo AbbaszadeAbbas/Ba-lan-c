9c64fba9d9235608be123d3419d27dca33e87b97
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-05-20 15:59:36 +0400

    python day_5 finished

---
+++ ProjectBackEnd/python/day_5/4) list() .py
@@ -0,0 +1,7 @@
+# # # #     list()  method
+
+# bu metoddan list yaratmaq ucun istifade olunur
+
+numbers = list(range(1,11));
+
+print(numbers);     # expected output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
